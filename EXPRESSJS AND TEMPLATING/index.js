66e649327e5e945241b75470eafc61efc1e31db0
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-09-28 12:08:20 +0300

    Add files via upload

--- EXPRESSJS AND TEMPLATING/index.js
+++ EXPRESSJS AND TEMPLATING/index.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const port = 8080;
-
-const users = [{
-    id: 1,
-    firstName: 'FirstName'
-}, {
-    id: 2,
-    firstName: 'FirstName 2'
-}, {
-    id: 3,
-    firstName: 'FirstName 3'
-}];
+const api = require('./api');
 
 const app = express();
 
-
+app.use(express.static(__dirname + '/public'));
 
 function defaultHandler(req, res) {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/views/index.html');
 }
-
-
-app.get('/user/:id', (req, res) => {
-    const user = users.find(u => u.id === +req.params.id);
-    res.send(user);
+app.use((req, res, next) => {
+    console.log('Time:', Date.now());
+    next();
 });
+
+app.use('/api', api);
+
+// app.get('/user/:id', (req, res) => {
+//     const user = users.find(u => u.id === +req.params.id);
+//     res.send(user || null);
+// });
 
 app.get('/', defaultHandler);
 
-
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send(err.message);
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
---
+++ EXPRESSJS AND TEMPLATING/users.js
@@ -0,0 +1,12 @@
+const users = [{
+    id: 1,
+    firstName: 'FirstName'
+}, {
+    id: 2,
+    firstName: 'FirstName 2'
+}, {
+    id: 3,
+    firstName: 'FirstName 3'
+}];
+
+module.exports = users;
