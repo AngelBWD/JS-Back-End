d54bc592371e9d04f48c6e144d52528a6c6e1b9f
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-09-30 23:40:03 +0300

    Add files via upload

--- WORKSHOP EXPRESSJS AND TEMPLATING/index.js
+++ WORKSHOP EXPRESSJS AND TEMPLATING/index.js
@@ -1,9 +1,19 @@
 const env = process.env.NODE_ENV || 'development';
 global.__basedir = __dirname;
-const config = require('./config/config')[env];
-const app = require('express')();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+const cubeModel = require('./models/cube')
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+cubeModel.insert({name: 'test1',desc: 'test1'}).then(insertedCube => {
+    console.log(insertedCube);
+    return cubeModel.delete(insertedCube.id)
+}).then((deletedCube) => {
+    console.log('deleted cube', deletedCube);
+    console.log('lastIndex shoud be 1 and we shoud not have any cubes');
+});
+// const config = require('./config/config')[env];
+// const app = require('express')();
+
+// require('./config/express')(app);
+// require('./config/routes')(app);
+
+// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
--- WORKSHOP EXPRESSJS AND TEMPLATING/models/cube.js
+++ WORKSHOP EXPRESSJS AND TEMPLATING/models/cube.js
@@ -1,7 +1,69 @@
 const data = require('../config/database');
+const fs = require('fs');
+const path = require('path');
+
 
 class CubeModel {
-   constructor(){
+
+   constructor() {
+      this.data = require('../config/database')
 
    }
+
+   _write(newData, resolveData){
+      return new Promise((resolve, reject) => {
+         fs.writeFile(path.resolve('config/database.json'), JSON.stringify(newData, null, 2), (err) => {
+            if (err) {reject(err); return; }
+            this.data = newData;
+            resolve(resolveData);
+         });
+      });
+   }
+   insert(newCube) {
+      const newIndex = ++this.data.lastIndex;
+      newCube = {id: newIndex, ...newCube };
+      const newData = {
+         lastIndex: newIndex,
+         entities: this.data.entities.concat(newCube)
+      };
+
+      return this._write(newData, newCube);
+   }
+
+   update(cubeId, update) {
+      const entityIndex = this.data.entities.findIndex(({id}) => id === cubeId);
+      const entity = this.data.entities[entityIndex];
+      const updateEntity = { ...entity, ...update};
+
+      const newData = {
+         lastIndex: this.data.lastIndex,
+         entities: [
+            ...this.data.entities.slice(0, entityIndex),
+            updateEntity,
+            ...this.data.entities.slice(entityIndex + 1)
+         ]
+      };
+      return this._write(newData, updateEntity);
+   }
+
+   delete(id) {
+      const deleteEntity = this.getOne(id);
+     
+      const newData = {
+         lastIndex: this.data.lastIndex,
+         entities: this.data.entities.filter(({id: i}) => i !== id)
+      }
+      return this._write(newData, deleteEntity);         
+   }
+
+   getOne() {
+      return Promise.resolve(this.data.entities.find(({id: i}) => i ===id)); 
+   }
+
+   getAll() {
+      return Promise.resolve(this.data.entities); 
+   }
+
 }
+
+module.exports = new CubeModel();
