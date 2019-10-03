7c12e7b015c0a75a34fd1b763a1c2009a39127fc
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-10-03 22:30:19 +0300

    Add files via upload

--- WORKSHOP EXPRESSJS AND TEMPLATING/index.js
+++ WORKSHOP EXPRESSJS AND TEMPLATING/index.js
@@ -1,19 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
 global.__basedir = __dirname;
 
-const cubeModel = require('./models/cube')
+const config = require('./config/config')[env];
+const app = require('express')();
 
-cubeModel.insert({name: 'test1',desc: 'test1'}).then(insertedCube => {
-    console.log(insertedCube);
-    return cubeModel.delete(insertedCube.id)
-}).then((deletedCube) => {
-    console.log('deleted cube', deletedCube);
-    console.log('lastIndex shoud be 1 and we shoud not have any cubes');
-});
-// const config = require('./config/config')[env];
-// const app = require('express')();
+require('./config/express')(app);
+require('./config/routes')(app);
 
-// require('./config/express')(app);
-// require('./config/routes')(app);
-
-// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
